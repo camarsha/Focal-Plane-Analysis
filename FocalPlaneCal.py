3c2c43463532e838a5b8c62089c72508f1fe41d9
Caleb <camarsha@ncsu.edu>
2017-05-25 15:45:48 -0400

    rho is log-normal, adding support

--- FocalPlaneCal.py
+++ FocalPlaneCal.py
@@ -25,7 +25,16 @@ def chi_square(poly,x_rho,x_channel,unc):
     temp = .5*((x_theory-x_rho)/unc)**2.0
     return np.sum(temp)
 
+#converts a standard deviation from a normal distribution to a log-normal sigma value
+#mu is normal distribution mean, sig is its s.d
+def log_normal_sigma(mu,sig):
+    log_sig = np.sqrt(np.log(1.+(sig/mu)**2.)) #equation can be found on Wikipedia
+    return log_sig
 
+#converts the normal distribution mean to the log-normal mu
+def log_normal_mu(mu,sig):
+    log_mu = np.log(mu/(np.sqrt(1.+(sig/mu)**2.)))
+    return log_mu
 
 # for all values with uncertainty
 def measured_value(x,dx):
@@ -59,7 +68,7 @@ class Nuclei():
 #class that handles all the kinematics
 class Reaction():
 
-    def __init__(self,a,A,b,B,B_field,E_lab,E_lab_unc):
+    def __init__(self,a,A,b,B,B_field,E_lab,E_lab_unc,theta):
         """
         Parse reaction names,looks up there masses, and calculates Q-value(MeV) 
         E_lab = MeV
@@ -75,7 +84,7 @@ class Reaction():
         self.B_field = B_field #mag field
         self.q = self.b.Z #charge of projectile
         self.E_lab = measured_value(E_lab,E_lab_unc)
-        self.theta = float(raw_input('What is the lab angle? \n'))#making the assumption this points are all from the same theta
+        self.theta = theta
         
         
         
@@ -89,6 +98,7 @@ class Focal_Plane_Fit():
         self.points = []
         self.fits = {}
         
+        
     def add_reaction(self):
         #take user input for reaction
         a = str(raw_input('Enter projectile \n'))
@@ -98,27 +108,30 @@ class Focal_Plane_Fit():
         B_field = float(raw_input('What was the B field setting? \n'))
         E_lab = float(raw_input('Beam energy? \n'))
         E_lab_unc = float(raw_input('Beam energy uncertainty? \n'))
-        self.reactions[len(self.reactions.keys())] = Reaction(a,A,b,B,B_field,E_lab,E_lab_unc) #keys for dictionary go from 0,..,n 
-        print 'Reaction',len(self.reactions.keys()),'has been defined as '+a+' + '+A+' -> '+B+' + '+b
+        theta = float(raw_input('What is the lab angle? \n'))#making the assumption this points are all from the same theta
+        self.reactions[len(self.reactions.keys())] = Reaction(a,A,b,B,B_field,E_lab,E_lab_unc,theta) #keys for dictionary go from 0,..,n 
+        print 'Reaction',(len(self.reactions.keys())-1),'has been defined as '+a+' + '+A+' -> '+B+' + '+b
         print 'E_beam =',E_lab,'+/- MeV',E_lab_unc,'With B-Field',B_field,'T' 
         
-
+    def add_point(self,reaction,level,level_unc,channel,channel_unc):
+        rho,rho_unc,rho_trace = self.calc_rho(reaction,level,level_unc) #get rho and uncertainty
+        rho = measured_value(rho,rho_unc) #convert to dict
+        channel = measured_value(channel,channel_unc) 
+        point = {'rho':rho,'channel':channel,'trace':rho_trace} 
+        self.points.append(point)
+        
     #add a calibration point which includes rho and an associated channel value    
-    def add_point(self):
+    def input_point(self):
         reaction = int(raw_input('Which reaction(0...n)? \n'))
         channel = float(raw_input('Enter the peak channel number. \n'))
         channel_unc = float(raw_input('What is the centroid uncertainty? \n'))
         level = float(raw_input('Enter the peak energy (MeV). \n'))
         level_unc = float(raw_input('Enter the peak uncertainty (MeV). \n'))
-        rho = self.calc_rho(reaction,level,level_unc)
-        channel = measured_value(channel,channel_unc)
-        point = {'rho':rho,'channel':channel}
-        self.points.append(point)
-
+        self.add_point(reaction,level,level_unc,channel,channel_unc)
     
         
     #added Monte Carlo error propagation for rho     
-    def calc_rho(self,reaction,E_level,E_level_unc):
+    def calc_rho(self,reaction,E_level,E_level_unc,steps=10000,burn=1000):
         reaction = self.reactions[reaction] #just for short hand pick out reaction
         E_level = pm.Normal('E_level',E_level,(1.0/E_level_unc)**2)#setup normal distribution for E_level
         reaction_variables = vars(reaction) #get variables from reaction
@@ -148,7 +161,7 @@ class Focal_Plane_Fit():
                      B_field=normals["B_field"],E_lab = normals["E_lab"],Q=normals["Q"]):
             #Based on Equation C.5 & C.6 for Christian's book.
             theta = theta*(np.pi/180.0) #to radians 
-            r = (np.sqrt(a*b*E_lab)/(b+B))*np.cos(theta)
+            r = (np.sqrt(a*b*E_lab)/(b+B))*np.cos(theta) 
             s = (E_lab*(B-a)+B*(Q-E_level))/(b+B)
             Eb =  (r + np.sqrt(r**2.0+s))**2.0 #We only care about positive solutions
 
@@ -168,11 +181,17 @@ class Focal_Plane_Fit():
         model_variables.append(rho_func)
         model = pm.Model(model_variables)
         mcmc = pm.MCMC(model) #prepare the sampler
-        mcmc.sample(50000,10000,) #50000 samples with a 10000 burn in 
-        print #blank line
-        print mcmc.stats()["rho_func"]
+        mcmc.sample(steps,burn) #50000 samples with a 10000 burn in
+        trace = mcmc.trace('rho_func')
+        print 
+        print "Sampling done, here are the stats"
+        print "The 95% confidence interval is",mcmc.stats()['rho_func']['95% HPD interval']
+        print "Mean:", mcmc.stats()['rho_func']['mean']
+        print "Standard Deviation:", mcmc.stats()['rho_func']['standard deviation']
+        return mcmc.stats()['rho_func']['mean'],mcmc.stats()['rho_func']['standard deviation'],trace #the stats we want
+        
      
-    #chi square fit 
+    #chi square fit will be good for quick cal 
     def fit(self,order=1):
         N = len(self.points) # Number of data points
         if N > (order+1): #check to see if we have n+2 points where n is the fit order
@@ -207,7 +226,30 @@ class Focal_Plane_Fit():
                 
         else:
             print "Not enough points to preform fit of order",order,"or higher"
-            
+
+    #now to try Bayesian fitting method          
+    def bay_fit(self,order):
+        #standard test to see if there are enough points
+        N = len(self.points)
+        if order < N-1:
+            print "Not enough points to preform fit of order",order,"or higher"
+        else:
+            #get data
+            x_obs = np.asarray([ele['channel']['value'] for ele in self.points])
+            x_unc = np.asarray([ele['channel']['unc'] for ele in self.points])
+            y_obs = np.asarray([ele['rho']['value'] for ele in self.points])
+            y_unc = np.asarray([ele['rho']['unc'] for ele in self.points])
+            #lets define priors for the polynomial terms
+            priors = []
+            for i in xrange(order+1):
+                priors.append(pm.Uniform(str(i),0,100)) #uniform priors from 0 to 100
+
+            #x and y likelihoods
+            x = pm.Normal('x',x_obs,(1.0/x_unc)**2.0)
+            y = pm.Normal('y',y_obs,(1.0/y_unc)**2.0)
+
+          
+    
     #finally given channel number use a fit to give energy         
     def peak_energy(self): 
         channel = float(raw_input("Enter channel number."))
@@ -218,4 +260,16 @@ class Focal_Plane_Fit():
             fit = 1
         print "Calibrated energy is:",self.fits[fit](channel)
 
-        
+    #simple function to read in a file with calibration points and preform a fit on them    
+    def read_calibration(self,cal_file,reaction=None):
+        #can just pick, or ask for user input for reaction
+        if type(reaction) != int:
+            reaction = int(raw_input('Which reaction(0...n)? \n'))
+        data = pd.read_csv(cal_file,sep='\s+')
+        for i in data.index:
+            level = data["level"][i]
+            level_unc = data["level_unc"][i]
+            channel = data["channel"][i]
+            channel_unc = data["channel_unc"][i]
+            self.add_point(reaction,level,level_unc,channel,channel_unc)
+        self.fit()
